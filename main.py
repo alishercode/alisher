c4cd215bbe0773daf7431115fdffda3a7e83081b
alishercode <aliataev9979@gmail.com>
2024-10-31 17:59:51 +0500

    add

---
+++ main.py
@@ -0,0 +1,803 @@
+# // console.log('Hello');
+# // console.log(123);
+# // console.log('city');
+# /////////////////////////////////////// vivod na consol///////////////////////////////
+
+# // let message = 'Hello'
+
+# // console.log(message);
+
+# // message = 'Bye'
+
+# // console.log(message);
+
+
+# // const message = 'Hello'
+
+# // message = 'Bye'
+
+# // const name = 'Ali'
+# // const age = 21
+# // const Software = true
+
+# // const admin = name
+
+# // console.log(admin);
+# // const name = 'Ali'
+
+
+# // const age = 18
+# // const age 20
+
+
+# // 'use strict'
+# // const age = 10
+
+# // console.log(age);
+# ////////////////////////////////////////////////////peremennie////////////////////////////
+
+
+# // const shouldlearnjs = true
+# // const isalreadylate = false
+
+# // const age = 38
+# // const ischild = age < 18
+
+# // console.log(ischild);
+
+
+# // let data = null 
+# // console.log(data);
+
+
+# // let name 
+# // console.log(name);
+
+
+# // const user = {}
+
+# // const message = 'Hello'
+
+# // console.log(
+# //     typeof message
+# // );
+
+# // console.log(
+# //     typeof  true, 111, 111n, {}, null
+# // );
+
+
+# // const num = 111
+# // const str = '222'
+# // const result = num + str
+
+# // console.log(result);
+# // console.log(typeof result);
+
+
+# // console.log(
+# //   typeof ('16' / '8')
+# // );
+
+
+# // const age = 11
+
+# // console.log(typeof age);
+# // console.log(typeof String(age));
+
+# // const isAdult = false
+
+# // console.log(isAdult);
+# // console.log(String(isAdult));
+
+# // const obj = {}
+# // name: 'Ali'
+# // console.log(
+# //     String(obj)
+# // );
+
+# // const str = '1000'
+# // const num = Number(str)
+
+# // console.log(num);
+# // console.log(typeof num);
+
+# // console.log(
+# //     Number('ne chislo')
+# // );
+
+
+# // console.log(
+# //     Number(true)
+# // );
+
+# // console.log(
+# //     Number(false)
+# // );
+
+# // console.log(
+# //     Number(null)
+# // );
+
+# // console.log(
+# //     Number(undefined)
+# // );
+
+
+# // console.log(Boolean(-1));
+# // console.log(Boolean(0));
+# // console.log(Boolean(1));
+# // console.log(Boolean(2));
+
+# // console.log(Boolean('Hello'));
+# // console.log(Boolean('0'));
+# // console.log(Boolean(' '));
+# // console.log(Boolean(''));
+
+# // console.log(Boolean(0));
+# // console.log(Boolean(NaN));
+# // console.log(Boolean(''));
+# // console.log(Boolean(null));
+# // console.log(Boolean(undefined));
+
+
+# // console.log(2 + 2);
+# // console.log(10 - 1);
+# // console.log(5 * 10);
+# // console.log(100 / 25);
+
+
+# // console.log(2 ** 10);
+
+
+# // console.log(13 % 5);
+# // console.log(5 % 2);
+
+# // console.log(-10);
+
+# // console.log(5 * -2 * 10 / -3);
+
+# // console.log(
+# //     typeof Number('123')
+# // );
+
+
+# // console.log(
+# //     typeof + '123'
+# // );
+
+
+# // console.log(+true);
+# // console.log(+false);
+# // console.log(+'123');
+# // console.log(+' ');
+# // console.log(+'');
+# // console.log(+null);
+# // console.log(+undefined);
+
+
+# // console.log(
+# //     +'3' +   +'5'
+# // );
+
+
+# // const result = 1 + -2 - 3 * + '4' / 5 ** 2
+
+# // console.log(result);
+
+
+# // let count = 1
+
+
+# // count += 2
+# // console.log(count);
+
+
+# // let count = 1
+
+# // count += 2
+# // count -= 5
+# // count *= 10
+# // count /= 3
+
+# // console.log(count);
+
+
+# // let message = 'Hello'
+# // message += ' Friend'
+# // message += ' How are you'
+
+# // console.log(message);
+
+# // let count = 10
+
+# // count-- 
+# // count++
+# // ++count
+# // console.log(count);
+
+
+# // let  count = 10
+# // let newCount = count++
+
+# // console.log(count);
+# // console.log(newCount);
+
+# // console.log(5 > 3);
+# // console.log(10 < 1);
+# // console.log(2 >= 2);
+# // console.log(3 <= 0);
+# // console.log(5 == 5);
+# // console.log(5 != 5);
+
+
+# // console.log('a' < 'b');
+# // console.log('ы' > 'f');
+# // console.log('Javascript' > 'Java');
+
+# // console.log(2 === '2');
+# // console.log('3' === 3);
+# // console.log(5 !== 10);
+
+# // const year = 3000
+
+# // if (year === 2024) {
+# //     console.log('Ura');
+# //     console.log('hell you tell me please javascript');
+# // } else  {
+# //     console.log('Great year =/');
+    
+# // }
+
+
+# // const year = 3000
+
+# // if (year === 2024) {
+# //     console.log('Ura');
+# // } else if (year === 3000) {
+# //     console.log('slishkom pozdno neyroset zamenila chelovechestvo:(');
+# // } else {
+# //     console.log('neizvestniy god:/');
+# // }
+
+
+# // const year = 2024
+# // let message = null
+
+# // if (year === 2024) {
+# //     message = 'nachinayu uchitsa!'
+# // } else {
+# //     message = 'neizvestniy god:/'
+# // }
+
+# // console.log(message);
+
+
+# // const year = 3000
+# // const message = year === 2024 
+# // ? 'nachinayu uchitsa!' 
+# // : 'neizvstniy god :/'
+
+# // console.log(message);
+
+
+# // const year = 3000
+# // const message = year === 2024
+# // ?'nachinayu uchitsa!'
+# // : year === 3000 
+# // ? 'neysroseti pobedili'
+# // : 'neizvestniy god :/'
+
+# // console.log(message);
+
+
+# // const age = 17 
+# // const withparent = false
+
+# // if (age > 18 || withparent) {
+# //     console.log('prohodite');
+# // } else {
+# //     console.log('nesovershennoletnim vhod zakrit ');
+# // }
+
+# // const result = '' || false || null || undefined || 0 || 'Hello' || 'Bye'
+
+# // console.log(result);
+
+
+# // const result = null || '' || 100 || 200 || 300
+
+# // console.log(result);
+
+
+# // console.log(false && false);
+# // console.log(true && false);
+# // console.log(false && true);
+# // console.log(true && true);
+
+
+# // const moneyInWallet = 5
+# // const iceCreamPrice = 2
+# // const isStoreOpen = false
+
+# // if (iceCreamPrice < moneyInWallet && isStoreOpen)
+# //     console.log('vot vashe morozhenoe!'); 
+# // else {
+# //     console.log('magazin zakrit:(');
+# // }
+
+# // const result = 'privet' && true && 5 && true && 10
+
+# // console.log(result);
+
+
+# // const age = 16
+# // const isYoung = age < 18
+
+# // isYoung && console.log('pokazhite passport');
+
+# // const age = 30 
+# // const isYoung = age < 18
+
+# // if (isYoung) {
+# //     console.log('pokazhite passport');
+# // }
+
+# // console.log(!true);
+# // console.log(!false);
+
+# // console.log('');
+# // console.log(!'Hello');
+# // console.log(!0);
+# // console.log();
+
+
+
+# // console.log(!!0);
+# // console.log(!!1);
+# // console.log(!!'');
+# // console.log(!!' ');
+
+# // console.log(Boolean(0));
+# // console.log(Boolean(1));
+# // console.log(Boolean(''));
+# // console.log(Boolean(' '));
+
+
+# // const name = 'Ali'
+# // const age = 21
+# // const hasMuchMoney = false
+# // const hasBoodJob = false
+# // const hasMotivation = true
+# // const hasFreeTime = true
+
+# // if (name !== 'Ali') {
+# //     console.log('vi ne Ali pozovite Ali');
+# // } else if (age < 16) {
+# //     console.log('eshe rano dumat o vzrosloy jizni');
+# // } else if (hasMuchMoney || hasBoodJob || hasMotivation && hasFreeTime) {
+# //     console.log('Ali nuzhno uchit FullStack');
+# // } else {
+# //     console.log('ti libo bezumno bogat chto mozhesh ne rabotat do konca jizni libo u tebya i tak horoshaya rabota libo tebe eto prosto ne nujno: (');
+# // }
+
+# // const a = 0 
+# // const b = 100
+
+# // const result1 = a || b
+# // const result2 = a ?? b
+
+# // console.log(result1);
+# // console.log(result2);
+
+
+# // alert('Hello');
+
+
+# // const message = 'Hello from message'
+
+# // alert(message)
+
+# // const message = 'Hello from message'
+
+# // alert(1 + 2 + 3)
+
+# // alert('Hello 1')
+# // console.log('Hello 1');
+# // alert('Hello 3')
+# // console.log('Hello 4');
+
+
+# // const userAge = prompt('How old a you?')
+
+# // console.log(`vozrast: ${userAge}`);
+
+
+# // const userAge = prompt('How old a you?, 18')
+
+# // if (userAge >= 18) {
+# //     console.log('dostup razreshen');
+# // } else {
+# //     console.log('dostup zapreshen');
+# // }
+
+# // const userAge = +prompt('How old a you?')
+ 
+# // if (userAge === 0) {
+# //     console.log('takogo vozrasta ne mozhet bit!');
+# // } else {
+# //     console.log(`tvoy vozrast: ${userAge}`);
+# // }
+
+# // const userAge = Number(prompt('How old a you?'))
+ 
+# // if (userAge === 0) {
+# //     console.log('takogo vozrasta ne mozhet bit!');
+# // } else {
+# //     console.log(`tvoy vozrast: ${userAge}`);
+# // }
+
+
+# // const isUserReady = confirm('Are you ready?')
+
+# // if (isUserReady) {
+# //     console.log('Lets go');
+# // } else {
+# //     console.log('Stop is it...');
+# // }
+
+# // const age = +prompt('How old a you?')
+
+# // switch (age) {
+# //     case 0: {
+# //         console.log('takogo vozrasta ne bivaet!');
+# //         break
+# //     }
+# //     case 18: {
+# //         console.log('ne veru pokazhi passport.');
+# //         break
+# //     }
+# //     case 1000: {
+# //         console.log('are you vampir?');
+# //         break
+# //     }
+# //     default: {
+# //         console.log(`tvoy vozrast: ${age}`);
+# //     }
+# // }
+
+# // const age = +prompt('How old a you?')
+
+# // switch (true) {
+# //     case age < 1: {
+# //         console.log('takogo vozrasta ne bivaet!');
+# //         break
+# //     }
+# //     case age === 18: {
+# //         console.log('ne veru pokazhi passport.');
+# //         break
+# //     }
+# //     case age > 0 && age <= 125: {
+# //         console.log(`tvoy vozrast: ${age}`);
+# //         break
+# //     }
+# //     case age > 125: {
+# //         console.log('are you vampir?');
+        
+# //     }
+# //     default: {
+# //         console.log('vozrast vveden nekkorektno');
+# //     }
+# // }
+
+# // function renderMenuItem(menuItem)  {}
+
+# // const menuItems = [
+# //     'glavnaya',
+# //     'katalog',
+# //     'kontakti',
+# //     'o nas',
+# // ]
+
+# // for (let i = 0; i < menuItems.length; i++) {
+# //     renderMenuItem(menuItems[i])
+# // }
+
+# // let password = ''
+
+# // while (password.length < 8) {
+# //     password = prompt('vvedite parol(ne menee 8 simvolov):')
+# // }
+# // console.log(`vash parol: ${password}`);
+
+
+# // let count = 100
+
+# // while (count < 10) {
+# //     console.log(count);
+# //     count++
+# // }
+
+# // let count = 0
+
+# // do {
+# //     console.log(count);
+# //     count++
+# // }
+# // while (count < 10) 
+
+# // let count = 0
+
+# // while(count < 10) {
+# //     console.log(count);
+# //     count++
+# // }
+
+# // for (let i = 0; i < 4; i++) {
+# //     alert(i)
+# // }
+
+# // console.log('cikl seychas nachnetsa....');
+
+# // for (let i = 0; i < 4;  i++) {
+# //     console.log(i);
+# //     const message = 'Hello'
+# // }
+
+# // console.log(message);
+
+# // console.log('...cikl zakonchen!');
+
+# // let count = 1
+
+# // console.log('cikl seychas nachnetsa...');
+
+# // while (count < 10) {
+# //     console.log(count);
+
+# //     if (count % 5 === 0) {
+# //         console.log('cikl prervan dosrochno');
+# //         break
+# //     }
+# //     count++
+# // }
+
+# // console.log('...cikl zakonchen!');
+
+
+# // console.log('cikl seychas nachnetsa...');
+
+# // for (let i = 0; i < 10; i++) {
+# //     console.log(i);
+
+# //     if (i === 3) {
+# //         console.log('cikl prervan dosrochno');
+# //         break
+# //     }
+# // }
+
+# // console.log('...cikl zakonchen');
+
+# // console.log('cikl seychas nachnetsa...');
+
+# // for (let i = 0; i < 10; i++) {
+# //     if (i % 2 === 0) {
+# //         continue
+# //     }
+# //     console.log(i);
+    
+# // }
+# // console.log('... cikl zakonchen!');
+
+
+# // console.log('Hello from consol!');
+
+# // const numberFromString = Number('100')
+# // const stringFromNumber = String(10)
+# // const booleanFromNull = Boolean(null)
+
+# // alert('Hello from alert')
+# // confirm('podtverzhdeniya deystviya')
+
+# // const answer = prompt('vvedite otvet:')
+
+# // function logHello() {
+# //     console.log('Hello');
+# // }
+# // logHello()
+# // logHello()
+# // logHello()
+
+# // const message = 'Hello'
+
+# // function logMessage() {
+    
+# //     console.log(message);
+# // }
+
+# // logMessage()
+
+# // console.log(`${message}, brother!`);
+
+
+# // const message = 'globalniy hello!'
+
+# // function logMessage() {
+# //     const message = 'lokalniy hello!'
+
+# //     console.log(message);
+
+# //     for (let i = 0; i < 3; i++) {
+# //         const message = `iteraciya cikla N${i}`
+# //         console.log(message);
+        
+# //     }
+# // }
+
+# // logMessage()
+
+# // console.log(message);
+
+
+# // {
+# //     const number = 1
+# //     console.log(number);
+
+# //     {
+# //     const number = 2
+# //     console.log(number);
+# //     {
+# //     const number = 3
+# //     console.log(number);
+# //         }
+# //     }
+
+# // }
+
+# // console.log(number);
+
+
+# // {
+# //     const number = 1
+# //     console.log(number);
+
+# //     {
+    
+# //     console.log(number);
+# //     {
+    
+# //     console.log(number);
+# //         }
+# //     }
+
+# // }
+
+# // console.log(number);
+
+# // function logMessage(message, count) {
+# //     for (let i = 0; i < count; i++) {
+# //         console.log(message);
+# //     }
+# // }
+
+# // logMessage('Hello!', 3)
+# // logMessage('Bye!', 2)
+
+# // console.log('Hello from consol');
+
+# // const numberFromString = Number('100')
+# // const stringFromNumber = String(10)
+# // const booleanFromNull = booleanFromNull(null)
+
+# // alert('hello from alert')
+# // confirm('podtverzhdeniya deystviya')
+
+# // const answer = prompt('vvedite otvet')
+
+# // function logHello() {
+# //     console.log('Hello');
+# // }
+
+# // logHello()
+
+# // function logMessage() {
+# //     const message = 'Hello'
+# //     console.log(message);
+# // }
+# // logMessage()
+
+# // console.log(`${message}, friend!`);
+
+# // const message = 'global hello!'
+
+# // function logMessage() {
+# //     const message = 'local hello'
+
+# //     console.log(message);
+
+# //     for (let i = 0; i < 3; i++) {
+# //         const message = `iterace cikla N${i}`
+# //         console.log(message);
+        
+# //     }
+    
+# // }
+
+# // logMessage()
+
+# // console.log(message);
+
+# // {
+# //     const number = 1
+# //     console.log(number);
+    
+# //     {
+# //     const number = 2
+# //     console.log(number);
+        
+# //         {
+# //     const number = 3
+# //     console.log(number);
+            
+# //         }
+# //     }
+# // }
+
+# // console.log(number);
+
+
+# // function logMessage(message, count) {
+# //     for (let i = 0; i < count; i++) {
+# //         console.log(message);
+        
+# //     }
+# // }
+
+# // logMessage(`Hello!`, 3)
+# // logMessage(`Bye!`, 2)
+
+# // const message = 'Hello'
+
+# // function logMessage(message, count) {
+# //     message = `(((${message})))`
+ 
+
+# //     for (let i = 0; i < count; i++) {
+# //         console.log(message);
+        
+# //     }
+# // }
+
+# // logMessage(message, 3)
+# // console.log(message);
+
+
+# // const globalMessage = 'Hello'
+
+# // function logMessage(message = 'myau', count) {
+# //    const messageFormated = `(((${message})))`
+ 
+
+# //     for (let i = 0; i < count; i++) {
+# //         console.log(messageFormated);
+        
+# //     }
+# // }
+
+# // logMessage(undefined, 5)
+ 
+# // function sumFiveNumbers(
+# //     num1 = 100,
+# //     num2 = 200,
+# //     num3 = 300,
+# //     num4 = 400,
+# //     num5,
+# // ) {
+# //     const sum = num1 + num2 + num3 + num4 + num5
+
+# //     console.log(`summa chisla ravna ${sum}`);
+    
+# // }
+
+# // sumFiveNumbers(4,4,4,4,4)
+
